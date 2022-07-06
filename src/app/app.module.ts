bf26a15b39c3dce442880e9c6ccdde179fcea990
InfamousRager <infamousragerrr@gmail.com>
2022-07-06 17:59:19 -0300

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatListModule } from '@angular/material/list' // adicionei pra parar de dar um erro que nao reconhecia o material list como component
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule, // adicionei pra parar de dar um erro que nao reconhecia o material list como component
   ],
   providers: [],
   bootstrap: [AppComponent]
